504964d250bd4d2d7a77aed44629102b12f2ea58
misPhi <1254sdxz@protonmail.com>
2026-03-07 12:00:30 +0900

    happy_numbers.c の更新

--- project/happy_numbers/happy_numbers.c
+++ project/happy_numbers/happy_numbers.c
@@ -14,18 +14,15 @@ int sum_squared_digits(int n) {
     return sum;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <number>\n", argv[0]);
-        return 1;
-    }
+int main() {
+    
 
-    int number = atoi(argv[1]);
+    int number = 19;
 
     int slow = number;
-    int fast = sum_squared_digits(number);
+    int fast = number;
 
-    while (fast != 1 && slow != fast) {
+    while (1) {
         slow = sum_squared_digits(slow);
         fast = sum_squared_digits(sum_squared_digits(fast));
     }

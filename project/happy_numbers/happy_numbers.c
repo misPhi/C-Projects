8c9b22537f1f5cb943e5d30c675f08971d8b0973
misPhi <1254sdxz@protonmail.com>
2026-03-11 22:49:44 +0900

    happy_numbers.c の更新

--- project/happy_numbers/happy_numbers.c
+++ project/happy_numbers/happy_numbers.c
@@ -25,8 +25,8 @@ int main() {
     while (1) {
         slow = sum_squared_digits(slow);
 printf("%d   ", slow);
-        fast = sum_squared_digits(sum_squared_digits(fast));
 
+        fast = sum_squared_digits(sum_squared_digits(fast));
 printf("%d\n", fast);
 
 if(slow == fast)
